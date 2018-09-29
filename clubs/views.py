69c664c32a66f9bf0110578daf83cf24d724785c
Shubham Goel <goelshubham17jan@gmail.com>
2018-09-29 10:43:02 +0530

    Dynamic Updation

--- clubs/views.py
+++ clubs/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
+import datetime
 
 import pyrebase
 #from pyrebase_settings import db,auth
@@ -84,4 +85,82 @@ def displayclub(request,pk):
 	for r,t in a.items():
 		if t["pk"]==pk:
 			ans=r
-	return render(request,"clubs/displayclub.html",{"clubname":ans,"details":a[ans]})
+	return render(request,"clubs/displayclub.html",{"clubname":ans,"details":db.child("clubs").child(ans).get().val()})
+
+# def addpic(request,pk):
+
+
+def desc(request,pk):
+	if request.method == "POST":
+		desc=request.POST.get('desc')
+		all_clubs = db.child("clubs").get()
+		a=all_clubs.val()
+		for r,t in a.items():
+			if t["pk"]==pk:
+				ans=r
+		db.child("clubs").child(ans).child(Desc).update(desc)
+		return render(request,'clubs/displayclub.html',{"clubname":ans,"details":db.child("clubs").child(ans).get().val()})	
+	return render(reuest,'clubs/description.html')
+
+def addannouncement(request,pk):
+	if request.method == "POST":
+		heading = request.POST.get('heading')
+		announcement = request.POST.get('announcement')
+		date = datetime.date.today()
+		now = datetime.datetime.now()
+		all_clubs = db.child("clubs").get()
+		a=all_clubs.val()
+		for r,t in a.items():
+			if t["pk"]==pk:
+				ans=r
+		ann=db.child("clubs").child(ans).child("Announcements").get().val()
+		if ann=="None":
+			r=[{"heading":heading,"announcement":announcement,"date":date,"time":time}]
+		else:
+			r=ann.append({"heading":heading,"announcement":announcement,"date":date,"time":time})
+		db.child("clubs").child(ans).child("Announcements").update(r)
+		return render(request,'clubs/displayclub.html',{"clubname":ans,"details":db.child("clubs").child(ans).get().val()})
+	return render(request,'clubs/addanouncement.html')
+
+def addevent(request,pk):
+	if request.method == "POST":
+		heading = request.POST.get('heading')
+		description = request.POST.get('description')
+		time = request.POST.get('time')
+		date = request.POST.get('date')
+		venue = request.POST.get('venue')
+		mob = request.POST.get('mob')		
+		all_clubs = db.child("clubs").get()
+		a=all_clubs.val()
+		for r,t in a.items():
+			if t["pk"]==pk:
+				ans=r
+		ann=db.child("clubs").child(ans).child("Events").get().val()
+		if ann=="None":
+			r=[{"heading":heading,"description":description,"date":date,"time":time,"venue":venue,"mob":mob}]
+		else:
+			r=ann.append({"heading":heading,"description":description,"date":date,"time":time,"venue":venue,"mob":mob})
+		db.child("clubs").child(ans).child("Events").update(r)
+		return render(request,'clubs/displayclub.html',{"clubname":ans,"details":db.child("clubs").child(ans).get().val()})
+	return render(request,'clubs/addevent.html')
+
+
+
+
+def addresource(request,pk):
+	if request.method == "POST":
+		heading = request.POST.get('heading')
+		src = request.POST.get('src')	
+		all_clubs = db.child("clubs").get()
+		a=all_clubs.val()
+		for r,t in a.items():
+			if t["pk"]==pk:
+				ans=r
+		ann=db.child("clubs").child(ans).child("Res").get().val()
+		if ann=="None":
+			r=[{"heading":heading,"src":src}]
+		else:
+			r=ann.append({"heading":heading,"src":src})
+		db.child("clubs").child(ans).child("Res").update(r)
+		return render(request,'clubs/displayclub.html',{"clubname":ans,"details":db.child("clubs").child(ans).get().val()})
+	return render(request,'clubs/addresource.html')
